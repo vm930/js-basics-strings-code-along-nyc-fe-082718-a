c732bc2dd74f95f65e21f51141fa7ff62731653f
abstract-animation-9914 <vm930@hotmail.com>
2018-10-02 01:05:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = "whatever i like";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
