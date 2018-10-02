0a2130079fb2799457551219e55d5467f682ce58
abstract-animation-9914 <vm930@hotmail.com>
2018-10-02 01:00:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// Write your code in this file!
+const currentUser = "whatever i like";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
